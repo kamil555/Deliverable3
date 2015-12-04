7fb9f96a2a612488d16775ca4dd50d74459bc1c5
gcglynn <gcglynn@gmail.com>
2015-12-04 10:38:35 -0800

    adding users

--- 360Group/src/main/Bidder.java
+++ 360Group/src/main/Bidder.java
@@ -1,5 +1,118 @@
 package main;
 
-public class Bidder {
+import java.io.IOException;
+import java.text.ParseException;
 
+import UI.*;
+
+/**
+ * 
+ * @author Stepan Adespya
+ * @since November 5, 2015
+ */
+public class Bidder{
+	
+	/**
+	 * Main menu for the Bidder User
+	 * 
+	 * @throws IOException
+	 * @throws ParseException
+	 * @throws ClassNotFoundException 
+	 * 
+	 */
+	public Bidder(User u) throws IOException, ParseException, ClassNotFoundException{
+		BidderInterface bi = new BidderInterface(u);
+		int input = bi.mainMenu();
+		switch (input){
+			case 1:
+				this.viewOpenAuctions(u,bi);
+				break;
+			case 2:
+				this.viewItemsBidded(u);
+				int select = bi.enterItemID();
+				Inventory i = new Inventory();
+				Item it = i.getItemFromList(select);
+				if (select == -1){
+					new Bidder(u);
+				}else{
+					double money = bi.editBidAmount();
+					editBid(u, it, money);
+					new Bidder(u);
+				}
+				
+				break;
+			case 3:
+				new UserInterface();
+				break;
+		}
+		
+	}
+	
+	/**
+	 * Lets you bid on a given item.
+	 * 
+	 * @param u
+	 * @param i
+	 * @throws IOException
+	 * @throws ParseException
+	 * @throws ClassNotFoundException 
+	 */
+	public void viewItem(User u, Item i,BidderInterface bi) throws IOException, ParseException, ClassNotFoundException{
+		System.out.println(i.toString());
+		int input = bi.viewItemMenu();
+		switch (input){
+			case 1:
+				double money = bi.enterBidAmount();
+				Bid b = new Bid(u.getUserName(), i.getItemID(), money);
+				bidList bl = new bidList();
+				bl.addBid(u, b);
+				new Bidder(u);
+				break;
+			case 2:
+				new Bidder(u);
+				break;
+		}
+	}
+	
+	/**
+	 * Allows you to change your bid on a given item.
+	 * 
+	 * @param u
+	 * @param i
+	 * @param money
+	 * @throws IOException
+	 */
+	public void editBid(User u, Item i, double money) throws IOException{
+		bidList b = new bidList();
+		b.editBid(u, i, money);
+	}
+	
+	/**
+	 * Shows the User all open auctions.
+	 * 
+	 * @param u
+	 * @throws ParseException
+	 * @throws IOException
+	 * @throws ClassNotFoundException 
+	 */
+	public void viewOpenAuctions(User u, BidderInterface bi) throws ParseException, IOException, ClassNotFoundException{
+		CalendarAuctionCentral c = new CalendarAuctionCentral();
+		int selectAuction = bi.selectAuction(c);
+		Auction a = c.getfutureAuctionList().get(selectAuction);
+		Inventory i = new Inventory();
+		int selectItem = bi.selectItem(i, a);
+		viewItem(u, i.listofItems.get(selectItem),bi);
+	}
+	
+	/**
+	 * Shows the all the Users bided items.
+	 * 
+	 * @param u
+	 * @throws IOException
+	 */
+	private void viewItemsBidded(User u) throws IOException{
+		Inventory i = new Inventory();
+		i.allItemsBidder(u);
+	}
+	
 }
--- 360Group/src/main/User.java
+++ 360Group/src/main/User.java
@@ -5,7 +5,8 @@ package main;
  * @author Stepan Adespya
  * @since November 5, 2015
  */
-public class User{
+@SuppressWarnings("serial")
+public class User implements java.io.Serializable{
 	// UserName of person
 	private String userName;
 	// Whether a person is a bidder,employee, or nonprofit
@@ -24,26 +25,62 @@ public class User{
 		this.user = user;
 	}
 	
+	/**
+	 * gives the User's Username
+	 * @return Username
+	 * (Precondition nothing)
+	 * (Postcondition returns Username)
+	 */
 	public String getUserName() {
 		return userName;
 	}
 
+	/**
+	 * Replaces the Username
+	 * @param userName
+	 * (Precondition String passed is not NULL)
+	 * (Postcondition replaces the Username)
+	 */
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
 
+	/**
+	 * States what the user is(Bidder,NonProfit, or Employee)
+	 * @return String of what the User is
+	 * (Precondition nothing)
+	 * (Postcondition returns state of User)
+	 */
 	public String getUser() {
 		return user;
 	}
 
+	/**
+	 * Sets the User to correct state.
+	 * @param user
+	 * (Precondition String is not NULL)
+	 * (Postcondition User becomes in right state)
+	 */
 	public void setUser(String user) {
 		this.user = user;
 	}
 
+	/**
+	 * Gives the Organization name if User is Nonprofit
+	 * @return String Organization name
+	 * (Precondition User is NonProfit)
+	 * (Postcondition returns Organization Name)
+	 */
 	public String getOrganization() {
 		return organization;
 	}
 
+	/**
+	 * Puts a new Organization name.
+	 * @param organization
+	 * (Precondition String is not Null)
+	 * (Postcondition Organization name to the right User)
+	 */
 	public void setOrganization(String organization) {
 		this.organization = organization;
 	}
