ef76ed50d0e7974cfdbe7e7be96f782b685a9d92
kamil555 <kamil555111@hotmail.com>
2015-12-05 18:16:27 -0800

    new commit
    new commit

--- 360Group/src/main/BidList.java
+++ 360Group/src/main/BidList.java
@@ -8,14 +8,27 @@ import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-
+/**
+ * 
+ * @author Han
+ * @edited by Stepan
+ */
 public class BidList{
 	public ArrayList<Bid> Bidlist;
-
+	/**
+	 * create a arraylist for bid and read from Bid.txt
+	 * @throws IOException
+	 */
 	public BidList() throws IOException{
 		Bidlist = new ArrayList<Bid>();
 		readFileToBid("Bids.txt");
 	}
+	/**
+	 * add a new bid info to arraylist and save to Bids.txt
+	 * @param user: get the user info from bider class
+	 * @param bid: create a new Bid list 
+	 * @throws IOException
+	 */
 
 	public void addBid(User user, Bid b) throws IOException{
 		Bid per = new Bid(user.getUserName(), b.getItemID(), b.getBidAmount());
@@ -41,7 +54,13 @@ public class BidList{
 			}
 		}
 	}
-
+	/**
+	 * edit the previews bid price
+	 * @param user: get the user info from bider class
+	 * @param bid: create a new Bid list 
+	 * @param bidAmount: bider's bid price 
+	 * @throws IOException
+	 */
 	public void editBid(User user, Item item, double bidAmount)throws IOException{
 		for (int i = 0; i < Bidlist.size(); i++)
 		{
@@ -60,7 +79,12 @@ public class BidList{
 
 		}
 	}
-
+	/**
+	 * Cancel the bid for this Auction
+	 * @param user: get the user info from bider class
+	 * @param item: which item bid that user want to cancel
+	 * @throws IOException
+	 */
 	public void cancelBid(User user, Item item) throws IOException{
 		for (int i = 0; i < Bidlist.size(); i++)
 		{
@@ -73,9 +97,12 @@ public class BidList{
 			}
 		}
 	}
-
-	public String isWinBid(Item item)
-	{
+	/**
+	 * check the winning bid price and username for the item
+	 * @param item item information 
+	 * @return user's name and bid amount
+	 */
+	public String isWinBid(Item item) {
 		int winner = 0;
 		for (int i = 0; i < Bidlist.size(); i++)
 		{
@@ -85,14 +112,18 @@ public class BidList{
 				winner = i;
 			}
 		}
-
 		return "userName is:" + Bidlist.get(winner).getuserName()
 				+ " Amount is:" + Bidlist.get(winner).getBidAmount();
 
 	}
-
-	private void writeToFile(String fileName, String content) throws IOException
-	{
+	
+	/**
+	 * write this array list to the Bid.txt
+	 * @param string write to the file
+	 * @param blist the array list
+	 * @throws IOException
+	 */
+	private void writeToFile(String fileName, String content) throws IOException {
 		// TODO Auto-generated method stub
 		FileWriter fw = new FileWriter(fileName, true);
 		PrintWriter pw = new PrintWriter(fw);
@@ -105,6 +136,11 @@ public class BidList{
 		}
 		pw.close();
 	}
+	/**
+	 * write all items to Bid.txt
+	 * @param string
+	 * @throws IOException
+	 */
 
 	private void writeAllItemsToFile(String string) throws IOException
 	{
@@ -117,7 +153,11 @@ public class BidList{
 		}
 		pw.close();
 	}
-
+	/**
+	 * Clear the file
+	 * @param string an empty string
+	 * @throws IOException
+	 */
 	private void clearFile(String string) throws IOException
 	{
 		FileWriter fw = new FileWriter(string, true);
@@ -125,7 +165,10 @@ public class BidList{
 		pw.print("");
 		pw.close();
 	}
-
+	/**
+	 * read file to the array list
+	 * @param fileName file' name 
+	 */
 	private void readFileToBid(String fileName)
 	{
 		String line = null;
---
+++ 360Group/src/test/BidListTest.java
@@ -0,0 +1,89 @@
+package test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import javax.jws.soap.SOAPBinding.Use;
+
+import main.Bid;
+import main.BidList;
+import main.Item;
+import main.User;
+
+import org.junit.Before;
+import org.junit.Test;
+/**
+ * 
+ * @author Han
+ *
+ */
+public class BidListTest {
+
+	private static final double TOLERANCE = .0001;
+
+	private BidList bidlist;
+	private User user;
+	private Item item;
+	private Bid bid;
+	//	private Inventory inv;
+
+
+	/**
+	 * set up before test
+	 * @throws Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		bidlist = new BidList();
+		user = new User("Han","User");
+		item = new Item(123, "123", 1.0, "cake", "cake");
+		bid = new Bid(user.getUserName(), 123, 4.2);
+	}
+	
+	/**
+	 * test Bidlist
+	 * 
+	 */
+	@Test
+	public void testBidList() {
+		assertTrue(bidlist.Bidlist != null);
+	}
+
+	/**
+	 * test addBid
+	 * @throws Exception
+	 */
+	@Test
+	public void testAddBid() throws Exception {
+		//		bidlist.addBid(user, bid);
+		assertEquals("add name fail", "Han", user.getUserName());
+		assertEquals("add ID fail", 123, item.getItemID());
+		assertEquals("add Amount fail", 4.2, bid.getBidAmount(),TOLERANCE);
+	}
+	/**
+	 * test edit bid
+	 * @throws Exception
+	 */
+	@Test
+	public void testEditBid() throws Exception {
+		assertEquals("edit name fail", "Han", user.getUserName());
+		assertEquals("add ID fail", 123, item.itemID);
+		assertEquals("edit Amount fail", 4.2, bid.getBidAmount(),TOLERANCE);
+	}
+	/**
+	 * test cancel bid
+	 */
+	@Test
+	public void testCancelBid() {
+		assertTrue(bid.getuserName().equalsIgnoreCase(user.getUserName()));
+		assertTrue(bid.getItemID() == item.itemID);
+	}
+	/**
+	 * test who is winning the bid
+	 */
+	@Test
+	public void testIsWinBid() {
+		bidlist.isWinBid(item);
+		assertEquals("error", "userName is:Han Amount is:4.2", bidlist.isWinBid(item).toString());
+	}
+
+}
