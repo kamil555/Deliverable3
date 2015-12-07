180e3870c938c9727105ad9549201eab103c51d7
kamil555 <kamil555111@hotmail.com>
2015-12-07 10:12:19 -0800

    new changes

--- 360Group/src/test/BidListTest.java
+++ 360Group/src/test/BidListTest.java
@@ -2,14 +2,14 @@ package test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import javax.jws.soap.SOAPBinding.Use;
+import java.io.IOException;
 
 import main.Bid;
 import main.BidList;
+import main.Inventory;
 import main.Item;
 import main.User;
 
-import org.junit.Before;
 import org.junit.Test;
 /**
  * 
@@ -22,31 +22,9 @@ public class BidListTest {
 
 	private BidList bidlist;
 	private User user;
+	private Inventory in;
 	private Item item;
 	private Bid bid;
-	//	private Inventory inv;
-
-
-	/**
-	 * set up before test
-	 * @throws Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-		bidlist = new BidList();
-		user = new User("Han","User");
-		item = new Item(123, "123", 1.0, "cake", "cake");
-		bid = new Bid(user.getUserName(), 123, 4.2);
-	}
-	
-	/**
-	 * test Bidlist
-	 * 
-	 */
-	@Test
-	public void testBidList() {
-		assertTrue(bidlist.Bidlist != null);
-	}
 
 	/**
 	 * test addBid
@@ -54,7 +32,15 @@ public class BidListTest {
 	 */
 	@Test
 	public void testAddBid() throws Exception {
-		//		bidlist.addBid(user, bid);
+		bidlist = new BidList();
+		in = new Inventory();
+		user = new User("Han","User");
+		item = new Item(123, "123", 1.0, "cake", "cake");
+		bid = new Bid(user.getUserName(), 123, 4.2);
+		in.addItem(item);
+		
+		bidlist.addBid(user, bid);
+	
 		assertEquals("add name fail", "Han", user.getUserName());
 		assertEquals("add ID fail", 123, item.getItemID());
 		assertEquals("add Amount fail", 4.2, bid.getBidAmount(),TOLERANCE);
@@ -65,25 +51,55 @@ public class BidListTest {
 	 */
 	@Test
 	public void testEditBid() throws Exception {
-		assertEquals("edit name fail", "Han", user.getUserName());
-		assertEquals("add ID fail", 123, item.itemID);
-		assertEquals("edit Amount fail", 4.2, bid.getBidAmount(),TOLERANCE);
+		bidlist = new BidList();
+		in = new Inventory();
+		user = new User("Han","User");
+		item = new Item(123, "123", 1.0, "cake", "cake");
+		bid = new Bid(user.getUserName(), 123, 4.2);
+		in.addItem(item);
+		
+		bidlist.addBid(user, bid);
+		bidlist.editBid(user, item, 5.5);
+		
+		assertEquals("edit Amount fail", "BidList =[Han,123,5.5]", bidlist.toString());
+		
 	}
 	/**
 	 * test cancel bid
+	 * @throws IOException 
 	 */
 	@Test
-	public void testCancelBid() {
+	public void testCancelBid() throws IOException {
+		bidlist = new BidList();
+		in = new Inventory();
+		user = new User("Han","User");
+		item = new Item(123, "123", 1.0, "cake", "cake");
+		bid = new Bid(user.getUserName(), 123, 4.2);
+		in.addItem(item);
+		
+		bidlist.cancelBid(user, item);
+		
 		assertTrue(bid.getuserName().equalsIgnoreCase(user.getUserName()));
-		assertTrue(bid.getItemID() == item.itemID);
+		assertTrue(bid.getItemID() == item.getItemID());
 	}
 	/**
 	 * test who is winning the bid
+	 * 
+	 * if there no Bids.txt,  run it again, it will be pass the test
+	 * 
+	 * @throws IOException 
 	 */
 	@Test
-	public void testIsWinBid() {
+	public void testIsWinBid() throws IOException {
+		bidlist = new BidList();
+		in = new Inventory();
+		user = new User("Han","User");
+		item = new Item(123, "123", 1.0, "cake", "cake");
+		bid = new Bid(user.getUserName(), 123, 4.2);
+		in.addItem(item);
+		bidlist.editBid(user, item, 5.5);
 		bidlist.isWinBid(item);
-		assertEquals("error", "userName is:Han Amount is:4.2", bidlist.isWinBid(item).toString());
+		assertEquals("error", "userName is:Han Amount is:5.5", bidlist.isWinBid(item).toString());
 	}
 
 }
